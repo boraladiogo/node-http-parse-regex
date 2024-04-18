dc260b825818f9e14ec6dd87d55480fe5af27ca5
Diogo Martins <boraladiogo@gmail.com>
2024-04-18 18:40:51 -0300

    Add basic CRUD functions to the database

--- src/database/database.js
+++ src/database/database.js
@@ -1,17 +1,67 @@
 import fs from 'node:fs/promises';
 
-const databasePath = new URL('../data.json', import.meta.url);
-
 export class Database {
     #database = {};
 
-    #persist() {
-        fs.writeFile(databasePath, JSON.stringify(this.#database));
+    #save() {
+        fs.writeFile(this.path, JSON.stringify(this.#database, null, 2));
     }
 
-    constructor() {
-        fs.readFile(databasePath, 'utf-8')
-            .then((data) => JSON.parse(data))
-            .catch(() => this.#persist());
+    #load() {
+        fs.readFile(this.path, 'utf-8')
+            .then((data) => { this.#database = JSON.parse(data); })
+            .catch(() => this.#save());
+    }
+
+    constructor(filename) {
+        this.path = new URL(`../${filename}`, import.meta.url);
+        this.#load();
+    }
+
+    select(table) {
+        return { rows: this.#database[table] ?? [] };
+    }
+
+    insert(table, data) {
+        if (Array.isArray(this.#database[table])) {
+            this.#database[table].push(data);
+        } else {
+            this.#database[table] = [data];
+        }
+
+        this.#save();
+
+        return { rows: this.#database[table][this.#database[table].length - 1] };
+    }
+
+    update(table, id, data) {
+        const updatedData = this.#database[table].reduce((rows, currentRow) => {
+            if (currentRow.id === id) {
+                rows.push({ ...currentRow, ...data });
+            } else {
+                rows.push(currentRow);
+            }
+            return rows;
+        }, []);
+
+        this.#database[table] = updatedData;
+        this.#save();
+
+        return { rows: updatedData };
+    }
+
+    delete(table, id) {
+        const updatedData = this.#database[table].reduce((rows, currentRow) => {
+            if (currentRow.id !== id) {
+                rows.push(currentRow);
+            }
+
+            return rows;
+        }, []);
+
+        this.#database[table] = updatedData;
+        this.#save();
+
+        return { rows: this.#database[table] };
     }
 }
