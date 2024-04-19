369ef1b6714f4a52399b845a75e8b487ab4ebcd6
Diogo Martins <boraladiogo@gmail.com>
2024-04-19 20:31:42 -0300

    Refact to better suit good practices - MVC Pattern

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,7 +1,9 @@
+import { userController } from './controllers/UserController.js';
+
 export const routes = [
     {
         method: 'GET',
         endpoint: '/users',
-        handler: (_req, res) => { res.writeHead(200).end(JSON.stringify({ message: 'HI!' })); },
+        handler: userController.index,
     },
 ];
